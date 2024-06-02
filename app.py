77cf2a1709d5bd90238eef413b93fe0128160da9
adamtarasiewicz <admtarasiewicz@gmail.com>
2024-06-02 12:54:48 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
 import json
 from fastapi.responses import JSONResponse
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 
 @app.get("/data")
-def read_data():
+def read_data(skip: int = 0, limit: int = 10):
     with open('rows.json') as f:
         data = json.load(f)
-    return JSONResponse(content=data)
+
+    total = len(data)
+    if skip >= total:
+        raise HTTPException(status_code=404, detail="Offset too large")
+
+    return JSONResponse(content={
+        "total": total,
+        "items": data[skip:skip + limit]
+    })
 
 
 if __name__ == "__main__":
